6e439041d117a784bd2816ae477c0d8c92791701
kushal-sw <kushalswargam484@gmail.com>
2026-01-13 10:16:54 +0530

    new commit

--- lec8.cpp
+++ lec8.cpp
@@ -1,5 +1,47 @@
+// #include <iostream>
+// using namespace std;
+
+// int factorial(int n) {
+//     int fact = 1;
+//     for (int i = 1; i <= n; i++) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// int main() {
+//     int num, temp, digit, sum = 0;
+//     cout << "enter a number :";
+//     cin >> num;
+
+//     temp = num;
+
+//     while (temp > 0) {
+//         digit = temp % 10;
+//         sum += factorial(digit);
+//         temp /= 10;
+//     }
+
+//     if (sum == num)
+//         cout << "Strong Number";
+//     else
+//         cout << "Not a Strong Number";
+
+//     return 0;
+// }
+
+
 #include <iostream>
+#include<cmath>
 using namespace std;
 int main(){
-    
+    int num, originalnum, remainder, n = 0;
+    double result = 0.0;
+    cout << "enter a number:::";
+    cin >> num;
+    originalnum = num;
+
+    while (originalnum  !=0)(
+        originalnum /= 10
+    );
 }
